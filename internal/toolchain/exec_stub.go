85772a1d49f5114c949ee0f6e818a607dfd2489e
dyam <dyam.marcano@gmail.com>
2025-03-31 14:59:02 -0300

    Add initial implementation of toolchain management for dynamic Go toolchain switching

---
+++ internal/toolchain/exec_stub.go
@@ -0,0 +1,11 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build js || wasip1
+
+package toolchain
+
+func execGoToolchain(gotoolchain, dir, exe string) {
+	base.Fatalf("execGoToolchain unsupported")
+}
