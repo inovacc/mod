53c90155f3e1437bf8b01522e5bfe95dc7372d6a
dyam <dyam.marcano@gmail.com>
2025-04-01 16:50:13 -0300

    base goroot 002

---
+++ cmd/cover/testdata/directives.go
@@ -0,0 +1,43 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This file is processed by the cover command, then a test verifies that
+// all compiler directives are preserved and positioned appropriately.
+
+//go:a
+
+//go:b
+package p
+
+//go:c1
+
+// doc
+//
+//go:c2
+func c() {
+}
+
+//go:d1
+
+// doc
+//
+//go:d2
+type d int
+
+//go:e1
+
+// doc
+//
+//go:e2
+type (
+	e int
+	f int
+)
+
+// go:_empty1
+// doc
+// go:_empty2
+type ()
+
+//go:f
